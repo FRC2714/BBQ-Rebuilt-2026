70c63068ba0e00d421a93202ea2e2b9d16c8ae7a
Kevin Liu <kvine2714@gmail.com>
2026-01-13 19:50:36 -0600

    Revert "updated drivetrain can ids"
    This reverts commit 81dfdb45e2165efdf57871b8184a1f22bdd06965.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,10 +46,10 @@ public final class Constants {
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 3;
-    public static final int kRearLeftDrivingCanId = 1;
+    public static final int kFrontLeftDrivingCanId = 7;
+    public static final int kRearLeftDrivingCanId = 3;
     public static final int kFrontRightDrivingCanId = 5;
-    public static final int kRearRightDrivingCanId = 7;
+    public static final int kRearRightDrivingCanId = 1;
 
     public static final int kFrontLeftTurningCanId = 8;
     public static final int kRearLeftTurningCanId = 4;

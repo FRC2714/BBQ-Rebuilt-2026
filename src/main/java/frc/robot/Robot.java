676b77a253abe817420457c56e2dcf015c5980d4
Ryan Juramongkol <nayarsun@outlook.com>
2026-01-16 21:03:01 -0600

    Avg Pose
    Co-Authored-By: Sanabhi <113478613+SanabhiG@users.noreply.github.com>
    Co-Authored-By: sohan-bhat <64918912+sohan-bhat@users.noreply.github.com>
    Co-Authored-By: Kailash Kainur <114515844+KKai27@users.noreply.github.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,9 +5,10 @@
 package frc.robot;
 
 import com.reduxrobotics.canand.CanandEventLoop;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.utils.LimelightHelpers;
@@ -34,8 +35,6 @@ public class Robot extends TimedRobot {
     m_robotContainer = new RobotContainer();
 
     CanandEventLoop.getInstance();
-
-
   }
 
   /**
@@ -53,13 +52,38 @@ public class Robot extends TimedRobot {
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
 
-        double omegaRps = Units.degreesToRotations(m_robotContainer.m_robotDrive.getTurnRate());
-    var llMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-front");
+    double omegaRps = Units.degreesToRotations(m_robotContainer.m_robotDrive.getTurnRate());
+    var frontLLMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-front");
+    var backLLMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-back");
 
-    if (llMeasurement != null && llMeasurement.tagCount > 0 && Math.abs(omegaRps) < 2.0) {
-      m_robotContainer.m_robotDrive.resetOdometry(llMeasurement.pose);
+    if (backLLMeasurement != null
+        && backLLMeasurement.tagCount > 0
+        && Math.abs(omegaRps) < 2.0
+        && backLLMeasurement.rawFiducials[0].ambiguity
+            <= frontLLMeasurement.rawFiducials[0].ambiguity) {
+      m_robotContainer.m_robotDrive.resetOdometry(backLLMeasurement.pose);
+    } else if (frontLLMeasurement != null
+        && frontLLMeasurement.tagCount > 0
+        && Math.abs(omegaRps) < 2.0) {
+      m_robotContainer.m_robotDrive.resetOdometry(frontLLMeasurement.pose);
     }
-    SmartDashboard.putNumber("tx", LimelightHelpers.getTX("limelight-front"));  
+
+    if (backLLMeasurement != null
+        && frontLLMeasurement != null
+        && frontLLMeasurement.tagCount > 0
+        && backLLMeasurement.tagCount > 0
+        && Math.abs(omegaRps) < 2.0) {
+      Pose2d avgPose =
+          new Pose2d(
+              (frontLLMeasurement.pose.getX() + backLLMeasurement.pose.getX()) / 2,
+              (frontLLMeasurement.pose.getY() + backLLMeasurement.pose.getY()) / 2,
+              (Rotation2d.fromDegrees(
+                  ((frontLLMeasurement.pose.getRotation()).getDegrees()
+                          + backLLMeasurement.pose.getRotation().getDegrees())
+                      / 2)));
+
+      // m_robotContainer.m_robotDrive.resetOdometry(avgPose);
+    }
   }
 
   /** This function is called once each time the robot enters Disabled mode. */

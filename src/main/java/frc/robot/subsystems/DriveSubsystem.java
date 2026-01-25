10850eaebf1bdebae5124550e02cd98bba417b95
sohan-bhat <sohanrambhat@gmail.com>
2026-01-25 12:54:43 -0600

    Refactor the math in drivesubsystem for shoot on the move

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -18,7 +18,6 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -27,7 +26,6 @@ import frc.robot.Constants.LimelightConstants;
 import frc.robot.utils.LimelightHelpers;
 
 public class DriveSubsystem extends SubsystemBase {
-  // Create MAXSwerveModules
   private final MAXSwerveModule m_frontLeft =
       new MAXSwerveModule(
           DriveConstants.kFrontLeftDrivingCanId,
@@ -52,12 +50,10 @@ public class DriveSubsystem extends SubsystemBase {
           DriveConstants.kRearRightTurningCanId,
           DriveConstants.kBackRightChassisAngularOffset);
 
-  // The gyro sensor
   private final Canandgyro m_gyro = new Canandgyro(0);
 
   private final Field2d m_field2d = new Field2d();
 
-  // Odometry class for tracking robot pose
   public SwerveDrivePoseEstimator m_poseEstimator =
       new SwerveDrivePoseEstimator(
           DriveConstants.kDriveKinematics,
@@ -72,18 +68,10 @@ public class DriveSubsystem extends SubsystemBase {
           LimelightConstants.m_stateStdDevs,
           LimelightConstants.m_visionStdDevs);
 
-  private Pose2d m_previousPose = new Pose2d();
-  private double m_previousTimestamp = 0;
-  private double m_robotVelocityX = 0;
-  private double m_robotVelocityY = 0;
+  private static final double kLatencyCompensation = 0.1;
+  private static final double kBaselineHorizontalVelocity = 6.0;
 
-  private static final double kShotSpeedMetersPerSecond = 12.0;
-  private static final double kDragCorrectionFactor = 1.15;
-  private static final double kMinLeadDistance = 0.5;
-
-  /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
-    // Usage reporting for MAXSwerve template
     HAL.report(tResourceType.kResourceType_RobotDrive, tInstances.kRobotDriveSwerve_MaxSwerve);
     SmartDashboard.putData("Field", m_field2d);
   }
@@ -121,37 +109,15 @@ public class DriveSubsystem extends SubsystemBase {
       }
     }
 
-    double currentTime = Timer.getFPGATimestamp();
-    Pose2d currentPose = m_poseEstimator.getEstimatedPosition();
-    double dt = currentTime - m_previousTimestamp;
-
-    if (dt > 0.005 && dt < 0.1) {
-      m_robotVelocityX = (currentPose.getX() - m_previousPose.getX()) / dt;
-      m_robotVelocityY = (currentPose.getY() - m_previousPose.getY()) / dt;
-    }
-
-    m_previousPose = currentPose;
-    m_previousTimestamp = currentTime;
-
     m_field2d.setRobotPose(m_poseEstimator.getEstimatedPosition());
     SmartDashboard.putNumber("heading", getHeading());
     SmartDashboard.putNumber("OdometryX", m_poseEstimator.getEstimatedPosition().getX());
   }
 
-  /**
-   * Returns the currently-estimated pose of the robot.
-   *
-   * @return The pose.
-   */
   public Pose2d getPose() {
     return m_poseEstimator.getEstimatedPosition();
   }
 
-  /**
-   * Resets the odometry to the specified pose.
-   *
-   * @param pose The pose to which to set the odometry.
-   */
   public void resetOdometry(Pose2d pose) {
     m_poseEstimator.resetPosition(
         Rotation2d.fromDegrees(getHeading()),
@@ -164,16 +130,7 @@ public class DriveSubsystem extends SubsystemBase {
         pose);
   }
 
-  /**
-   * Method to drive the robot using joystick info.
-   *
-   * @param xSpeed Speed of the robot in the x direction (forward).
-   * @param ySpeed Speed of the robot in the y direction (sideways).
-   * @param rot Angular rate of the robot.
-   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
-   */
   public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-    // Convert the commanded speeds into the correct units for the drivetrain
     double xSpeedDelivered = xSpeed * DriveConstants.kMaxSpeedMetersPerSecond;
     double ySpeedDelivered = ySpeed * DriveConstants.kMaxSpeedMetersPerSecond;
     double rotDelivered = rot * DriveConstants.kMaxAngularSpeed;
@@ -195,7 +152,6 @@ public class DriveSubsystem extends SubsystemBase {
     m_rearRight.setDesiredState(swerveModuleStates[3]);
   }
 
-  /** Sets the wheels into an X formation to prevent movement. */
   public void setX() {
     m_frontLeft.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
     m_frontRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(-45)));
@@ -203,11 +159,6 @@ public class DriveSubsystem extends SubsystemBase {
     m_rearRight.setDesiredState(new SwerveModuleState(0, Rotation2d.fromDegrees(45)));
   }
 
-  /**
-   * Sets the swerve ModuleStates.
-   *
-   * @param desiredStates The desired SwerveModule states.
-   */
   public void setModuleStates(SwerveModuleState[] desiredStates) {
     SwerveDriveKinematics.desaturateWheelSpeeds(
         desiredStates, DriveConstants.kMaxSpeedMetersPerSecond);
@@ -217,7 +168,6 @@ public class DriveSubsystem extends SubsystemBase {
     m_rearRight.setDesiredState(desiredStates[3]);
   }
 
-  /** Resets the drive encoders to currently read a position of 0. */
   public void resetEncoders() {
     m_frontLeft.resetEncoders();
     m_rearLeft.resetEncoders();
@@ -225,7 +175,6 @@ public class DriveSubsystem extends SubsystemBase {
     m_rearRight.resetEncoders();
   }
 
-  /** Zeroes the heading of the robot. */
   public void zeroHeading() {
     Pose2d pose = new Pose2d();
 
@@ -242,55 +191,30 @@ public class DriveSubsystem extends SubsystemBase {
         pose);
   }
 
-  /**
-   * Returns the heading of the robot.
-   *
-   * @return the robot's heading in degrees, from -180 to 180
-   */
   public double getHeading() {
     return m_gyro.getYaw() >= 0 ? m_gyro.getYaw() * 360 : Math.abs((m_gyro.getYaw() * 360) + 360);
   }
 
-  /**
-   * Returns the turn rate of the robot.
-   *
-   * @return The turn rate of the robot, in degrees per second
-   */
   public double getTurnRate() {
     return m_gyro.getAngularVelocityYaw() * 360 * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
   }
 
-  public Translation2d getRobotVelocity() {
-    return new Translation2d(m_robotVelocityX, m_robotVelocityY);
+  public ChassisSpeeds getRobotRelativeSpeeds() {
+    return DriveConstants.kDriveKinematics.toChassisSpeeds(
+        m_frontLeft.getState(),
+        m_frontRight.getState(),
+        m_rearLeft.getState(),
+        m_rearRight.getState());
   }
 
-  public double getRobotSpeed() {
-    return Math.hypot(m_robotVelocityX, m_robotVelocityY);
+  public Translation2d getFieldRelativeVelocity() {
+    ChassisSpeeds robotSpeeds = getRobotRelativeSpeeds();
+    Rotation2d heading = Rotation2d.fromDegrees(getHeading());
+    return new Translation2d(robotSpeeds.vxMetersPerSecond, robotSpeeds.vyMetersPerSecond)
+        .rotateBy(heading);
   }
 
   public double getAngleToHub() {
-    Pose2d pose = getPose();
-
-    // Hub position in field coordinates (meters)
-    Translation2d hub =
-        new Translation2d(
-            edu.wpi.first.math.util.Units.inchesToMeters(468.56),
-            edu.wpi.first.math.util.Units.inchesToMeters(158.32));
-
-    // Vector from robot to hub
-    Translation2d robotToHub = hub.minus(pose.getTranslation());
-
-    // Field-relative angle to hub
-    Rotation2d angleToHub = robotToHub.getAngle();
-
-    // Turret angle relative to robot forward
-    Rotation2d turretAngle = angleToHub.minus(pose.getRotation());
-
-    // Return degrees (wrapped to [-180, 180])
-    return turretAngle.getDegrees();
-  }
-
-  public double getAngleToHubWithLead() {
     Pose2d pose = getPose();
 
     Translation2d hub =
@@ -299,23 +223,31 @@ public class DriveSubsystem extends SubsystemBase {
             Units.inchesToMeters(158.32));
 
     Translation2d robotToHub = hub.minus(pose.getTranslation());
-    double distanceToHub = robotToHub.getNorm();
+    Rotation2d angleToHub = robotToHub.getAngle();
+    Rotation2d turretAngle = angleToHub.minus(pose.getRotation());
 
-    if (distanceToHub < kMinLeadDistance) {
-      return getAngleToHub();
-    }
+    return turretAngle.getDegrees();
+  }
 
-    double flightTime = (distanceToHub / kShotSpeedMetersPerSecond) * kDragCorrectionFactor;
+  public double getAngleToHubWithLead() {
+    Pose2d pose = getPose();
+    Translation2d robotVelocity = getFieldRelativeVelocity();
 
-    Translation2d leadOffset =
+    Translation2d hub =
         new Translation2d(
-            m_robotVelocityX * flightTime,
-            m_robotVelocityY * flightTime);
+            Units.inchesToMeters(468.56),
+            Units.inchesToMeters(158.32));
 
-    Translation2d aimPoint = hub.minus(leadOffset);
-    Translation2d robotToAimPoint = aimPoint.minus(pose.getTranslation());
-    Rotation2d angleToAimPoint = robotToAimPoint.getAngle();
-    Rotation2d turretAngle = angleToAimPoint.minus(pose.getRotation());
+    Translation2d futurePosition =
+        pose.getTranslation().plus(robotVelocity.times(kLatencyCompensation));
+
+    Translation2d toGoal = hub.minus(futurePosition);
+    Translation2d targetDirection = toGoal.div(toGoal.getNorm());
+
+    Translation2d targetVelocity = targetDirection.times(kBaselineHorizontalVelocity);
+    Translation2d shotVelocity = targetVelocity.minus(robotVelocity);
+
+    Rotation2d turretAngle = shotVelocity.getAngle().minus(pose.getRotation());
 
     return turretAngle.getDegrees();
   }

bddbf87b7cf3aaa1c6992eb04b94975b05e43bb2
Ryan Juramongkol <nayarsun@outlook.com>
2026-02-01 11:38:31 -0600

    spotless
    Co-Authored-By: sohan-bhat <64918912+sohan-bhat@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -265,8 +265,9 @@ public class DriveSubsystem extends SubsystemBase {
    * @return the robot's heading in degrees, from -180 to 180
    */
   public double getHeading() {
-    return m_poseEstimator == null ? Units.rotationsToDegrees(m_gyro.getYaw()):
-    m_poseEstimator.getEstimatedPosition().getRotation().getDegrees();
+    return m_poseEstimator == null
+        ? Units.rotationsToDegrees(m_gyro.getYaw())
+        : m_poseEstimator.getEstimatedPosition().getRotation().getDegrees();
   }
 
   /**

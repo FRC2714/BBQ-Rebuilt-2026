285f6687eb3038830d73a83f98b53d6f28874cf2
Ryan Juramongkol <nayarsun@outlook.com>
2026-02-08 17:01:39 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -80,10 +80,10 @@ public class RobotContainer {
         .start()
         .onTrue(new InstantCommand(() -> m_robotDrive.zeroDriverHeading(), m_robotDrive));
 
-    m_driverController.rightBumper().onTrue(m_robotDrive.translationalQuasistatic());
-    m_driverController.leftBumper().onTrue(m_robotDrive.rotationalQuasistatic());
-    m_driverController.x().onTrue(m_robotDrive.translationalDynamic());
-    m_driverController.y().onTrue(m_robotDrive.rotationalDynamic());
+    // m_driverController.rightBumper().onTrue(m_robotDrive.translationalQuasistatic());
+    // m_driverController.leftBumper().onTrue(m_robotDrive.rotationalQuasistatic());
+    // m_driverController.x().onTrue(m_robotDrive.translationalDynamic());
+    // m_driverController.y().onTrue(m_robotDrive.rotationalDynamic());
   }
 
   /**

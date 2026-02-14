695c5740ae45cdb3515e4ca8b5137410f92893ad
Nitin Keshavan <nitinteri@gmail.com>
2026-02-14 12:30:07 -0600

    added stow sequence as a separate command
    Co-Authored-By: Sanabhi <113478613+SanabhiG@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -86,14 +86,11 @@ public class RobotContainer {
     m_driverController.a().onTrue(m_shooter.startShooter());
 
     // intake keybinds
-    m_driverController
-        .leftBumper()
-        .whileTrue(m_stateMachine.intakeSequence())
-        .whileFalse(m_stateMachine.stowSequence());
-    m_driverController
-        .rightBumper()
-        .whileTrue(m_stateMachine.extakeSequence())
-        .whileFalse(m_stateMachine.stowSequence());
+    m_driverController.leftBumper().whileTrue(m_stateMachine.intakeSequence());
+
+    m_driverController.rightBumper().whileTrue(m_stateMachine.extakeSequence());
+
+    m_driverController.b().whileTrue(m_stateMachine.stowSequence());
 
     // m_driverController.rightBumper().onTrue(m_robotDrive.translationalQuasistatic());
     // m_driverController.leftBumper().onTrue(m_robotDrive.rotationalQuasistatic());

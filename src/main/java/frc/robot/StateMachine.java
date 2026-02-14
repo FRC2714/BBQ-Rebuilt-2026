389750ff7fc6c670a34538e1d26ca1b586b260b7
Ryan Juramongkol <nayarsun@outlook.com>
2026-02-14 14:10:31 -0600

    spotless

--- src/main/java/frc/robot/StateMachine.java
+++ src/main/java/frc/robot/StateMachine.java
@@ -3,7 +3,6 @@ package frc.robot;
 import com.revrobotics.spark.SparkLimitSwitch;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;

6b113185b65ede0762c23a43e55b115ba8f0e206
Ryan Juramongkol <nayarsun@outlook.com>
2026-03-01 16:02:09 -0600

    remove depracated command, hope it builds....
    Co-Authored-By: Kailash Kainur <114515844+KKai27@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -34,6 +34,7 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -549,20 +550,21 @@ public class DriveSubsystem extends SubsystemBase {
     }
 
     // Switch back to fused mode after seeding
-    new WaitCommand(0.1)
-        .andThen(
-            new InstantCommand(
-                () -> LimelightHelpers.SetIMUMode(LimelightConstants.kFrontRightName, 4)))
-        .alongWith(
-            new InstantCommand(
-                () -> LimelightHelpers.SetIMUMode(LimelightConstants.kFrontLeftName, 4)))
-        .alongWith(
-            new InstantCommand(
-                () -> LimelightHelpers.SetIMUMode(LimelightConstants.kRearLeftName, 4)))
-        .alongWith(
-            new InstantCommand(
-                () -> LimelightHelpers.SetIMUMode(LimelightConstants.kRearRightName, 4)))
-        .schedule();
+    CommandScheduler.getInstance()
+        .schedule(
+            new WaitCommand(0.1)
+                .andThen(
+                    new InstantCommand(
+                        () -> LimelightHelpers.SetIMUMode(LimelightConstants.kFrontRightName, 4)))
+                .alongWith(
+                    new InstantCommand(
+                        () -> LimelightHelpers.SetIMUMode(LimelightConstants.kFrontLeftName, 4)))
+                .alongWith(
+                    new InstantCommand(
+                        () -> LimelightHelpers.SetIMUMode(LimelightConstants.kRearLeftName, 4)))
+                .alongWith(
+                    new InstantCommand(
+                        () -> LimelightHelpers.SetIMUMode(LimelightConstants.kRearRightName, 4))));
   }
 
   /** Sets the current heading as the driver's forward direction. */

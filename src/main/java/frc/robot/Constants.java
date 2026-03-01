cd9d2b277ab2a3f0c5e1dd8a07e8d331a9a98a1a
Ryan Juramongkol <nayarsun@outlook.com>
2026-03-01 15:01:52 -0600

    caps limit
    Co-Authored-By: Kailash Kainur <114515844+KKai27@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -123,10 +123,10 @@ public final class Constants {
   }
 
   public static final class LimelightConstants {
-    public static final String kFrontRightName = "limelight-frontR";
-    public static final String kFrontLeftName = "limelight-frontL";
-    public static final String kRearLeftName = "limelight-rearL";
-    public static final String kRearRightName = "limelight-rearR";
+    public static final String kFrontRightName = "limelight-fr";
+    public static final String kFrontLeftName = "limelight-fl";
+    public static final String kRearLeftName = "limelight-rl";
+    public static final String kRearRightName = "limelight-rr";
     public static final String[] kCameraNames = {
       kFrontRightName, kFrontLeftName, kRearLeftName, kRearRightName
     };

68ed9069e9a84e7f5fbfea228704660900885398
BBQ <nayarsun@outlook.com>
2026-03-05 22:35:56 -0600

    new pose pipelienes
    Co-Authored-By: Kevin Liu <128659871+kvine11@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -34,9 +34,9 @@ public final class Constants {
     public static final double kBumperThickness = Units.inchesToMeters(3.5); // Needs to change
 
     // Chassis configuration
-    public static final double kTrackWidth = Units.inchesToMeters(26.5);
+    public static final double kTrackWidth = Units.inchesToMeters(21.5);
     // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(20.5);
+    public static final double kWheelBase = Units.inchesToMeters(26);
     // Distance between front and back wheels on robot
     public static final SwerveDriveKinematics kDriveKinematics =
         new SwerveDriveKinematics(
